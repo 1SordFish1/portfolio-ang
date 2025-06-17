e7b407ce8d517cec46e14740b681696945d54ffe
Achuothan M <achuothan.m@centizen.com>
2025-06-17 18:39:01 +0530

    hello

--- src/app/core/home/home.ts
+++ src/app/core/home/home.ts
@@ -1,6 +1,8 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { gsap } from "gsap"
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { SplitText } from 'gsap/SplitText';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +12,25 @@ import { gsap } from "gsap"
 })
 export class Home {
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
+      gsap.registerPlugin(ScrollTrigger);
+      gsap.registerPlugin(SplitText);
       requestAnimationFrame(() => {
         gsap.timeline({ repeat: -1, yoyo: false, repeatDelay: 0 })
           .add(gsap.set(".gradient-text", { backgroundPositionX: 0 }))
-          .add(gsap.to(".gradient-text", { ease: "none", duration: 6, backgroundPositionX: 200 }))
+          .add(gsap.to(".gradient-text", { ease: "none", duration: 6, backgroundPositionX: 200 }));
+
+        let tlIntro = gsap.timeline({
+          scrollTrigger: {
+            trigger: "hello-container",
+            scrub: 2,
+            pin: true,
+          }
+        });
+
+        tlIntro.to('.hello-text', { scale: 130, xPercent: -250, transaformOrigin: "50% 50%" })
       });
     }
   }
