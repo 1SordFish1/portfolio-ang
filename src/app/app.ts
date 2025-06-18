3ea08e8acdfe46529e430772fe66136b4dceb143
Achuothan M <achuothan.m@centizen.com>
2025-06-18 12:12:08 +0530

    loader

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { gsap } from "gsap"
+import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,53 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'portfolio-ang';
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      gsap.registerPlugin(ScrambleTextPlugin);
+      const chars = "QWERTYUIOPLKJHGFDSAZXCVBNMplmnkoijbhuygvcftrdxzsewqa6574839201*&^%$#@!"
+
+      requestAnimationFrame(() => {
+        let loaderTimeline = gsap.timeline({
+          delay: 0,
+          repeat: 0,
+          repeatDelay: 1,
+          repeatRefresh: true,
+          smoothChildTiming: true,
+          autoRemoveChildren: true,
+        });
+
+        loaderTimeline.set(".loader", {opacity: 0});
+        loaderTimeline.to(".loader", {opacity: 1});
+
+        loaderTimeline.to("#loader-text", {
+          delay: 2,
+          duration: 2,
+          scrambleText: {
+            text: "Ready to Go",
+            chars: chars,
+            tweenLength: true,
+            speed: 1,
+            delimiter: " "
+          }
+        });
+
+        loaderTimeline.to("#app-loader", {
+          scaleY: 0,
+          opacity: 0,
+          transformOrigin: "50% 0%",
+          delay: 1,
+          duration: 1,
+          ease: "power4.out",
+          onComplete: () => {
+            const loader = document.querySelector('#app-loader');
+            if (loader) {
+              loader.remove()
+            }
+          }
+        });
+      });
+    }
+  }
 }
